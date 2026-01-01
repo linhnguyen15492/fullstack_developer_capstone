a8875ccc294e3482cb191acd13dfc4522167ee97
Linh Nguyen <honglinh154@gmail.com>
2026-01-01 06:05:23 +0000

    reformat code

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -87,11 +87,11 @@ def registration(request):
         )
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username, "status": 200, 
+        data = {"userName": username, "status": 200,
                 "message": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName": username, "status": 400, 
+        data = {"userName": username, "status": 400,
                 "message": "Already Registered"}
         return JsonResponse(data)
 
@@ -104,7 +104,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, 
+                     "CarMake": car_model.car_make.name})
 
     return JsonResponse({"CarModels": cars})
 
